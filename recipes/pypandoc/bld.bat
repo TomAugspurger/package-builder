c0073ed13670b3dcde8441c659b8bd42932ea7ad
Jan Schulz <jasc@gmx.net>
2015-10-22 14:27:17 +0200

    Add darwin wheels and fix PyPI upload

--- recipes/pypandoc/bld.bat
+++ recipes/pypandoc/bld.bat
@@ -3,12 +3,25 @@ if errorlevel 0 (
    echo "Build wheels..."
    "%PYTHON%" setup.py bdist_wheel
    echo "Upload wheels..."
-   twine upload -u %PYPI_USER% -p %PYPI_PASS% --config-file %~dp0\pypirc -r pypitest dist/pypandoc-*.whl
+   (
+    @echo [distutils] # this tells distutils what package indexes you can push to
+    @echo index-servers =
+    @echo     pypi
+    @echo    pypitest
+    @echo. 
+    @echo [pypi] # authentication details for live PyPI
+    @echo repository: https://pypi.python.org/pypi
+    @echo. 
+    @echo [pypitest] # authentication details for test PyPI
+    @echo repository: https://testpypi.python.org/pypiuse this file to run your own startup commands 
+    ) > "pypirc"
+   twine upload -u %PYPI_USER% -p %PYPI_PASS% --config-file pypirc -r pypitest dist/pypandoc-*.whl
 )
-echo "Cleanupwheel build..."
+echo "Cleanup wheel build..."
 "%PYTHON%" setup.py clean
 del /q pypandoc\files\*.*
 del /q pandoc-*-windows.msi
+del /q pypirc
 
 "%PYTHON%" setup.py install --single-version-externally-managed  --record record.txt
 if errorlevel 1 exit 1
--- recipes/pypandoc/build.sh
+++ recipes/pypandoc/build.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+$PYTHON setup.py download_pandoc
+if [ $? -eq 0 ]
+then
+  echo "Build wheels..."
+  $PYTHON setup.py bdist_wheel
+  echo "Upload wheels..."
+  cat << EOF > pypirc
+[distutils] # this tells distutils what package indexes you can push to
+index-servers =
+    pypi
+    pypitest
+
+[pypi] # authentication details for live PyPI
+repository: https://pypi.python.org/pypi
+
+[pypitest] # authentication details for test PyPI
+repository: https://testpypi.python.org/pypi
+EOF
+  twine upload -u %PYPI_USER% -p %PYPI_PASS% --config-file pypirc -r pypitest dist/pypandoc-*.whl
+fi
+echo "Cleanup wheel build..."
+$PYTHON setup.py clean
+rm pypandoc\files\*.*
+rm pandoc-*-osx.pkg
+rm pandoc-*-amd64.deb
+rm pypirc
 
 $PYTHON setup.py install --single-version-externally-managed  --record record.txt
 
